eef6ee45d1462ab16b681e6898f0955b1b97cdbc
moideenubais <moideenubais@gmail.com>
2021-11-02 22:56:51 +0530

    removed commented codes

--- routes/user.js
+++ routes/user.js
@@ -5,19 +5,8 @@ const user = require("../controllers/user");
 const validateObjectId = require("../middleware/validateObjectId");
 const auth = require("../middleware/auth");
 
-router
-  .route("/")
-  // .all(auth, policy.isAllowed) //.all(kHubPolicy.isAllowed) need to include policy
-  //   .get(auth, policy.isAllowed, user.getUsers)
-  .post(user.postUser);
+router.route("/").post(user.postUser);
 
 router.route("/analytics").get(auth, user.getAnalytics);
-
-// router
-//   .route("/:id")
-//   .all(validateObjectId) //add auth and policy
-//   .get(auth, policy.isAllowed, user.getSingleUser)
-//   .put([auth, policy.isAllowed, upload.single("image")], user.updateUser)
-//   .delete(auth, policy.isAllowed, user.deleteUser);
 
 module.exports = router;
